c28ef76719e67a787152fb0199a4924374dfebd0
enset <meg@enset.ru>
2017-04-05 20:47:22 +0300

    NavigationBar, to page views

--- RBWLFinal/ViewController.swift
+++ RBWLFinal/ViewController.swift
@@ -8,18 +8,79 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout  {
 
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
+        cv.backgroundColor = .green
+        cv.dataSource = self
+        cv.delegate =  self
+        cv.isPagingEnabled = true
+        cv.bounces = false
+        return cv
+    }()
+    
+    
+    let navigationBar: UINavigationBar = {
+        let nb = UINavigationBar()
+        nb.isTranslucent = false
+        nb.barTintColor = .black
+        let navItem = UINavigationItem(title: "RBWL")
+        
+        nb.setItems([navItem], animated: false)
+        nb.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        
+        return nb
+    }()
+    
+    let statusBarView: UIView = {
+        let sbv = UIView()
+        sbv.backgroundColor = .black
+        return sbv
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(collectionView)
+        view.addSubview(statusBarView)
+        view.addSubview(navigationBar)
+        
+        statusBarView.anchor(view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 22)
+        
+        navigationBar.anchor(statusBarView.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 48)
+        collectionView.anchorToTop(view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor)
+        
+        
+        collectionView.register(PageCell.self, forCellWithReuseIdentifier: cellId)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    let cellId = "cellId"
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+   
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width:view.frame.width, height: view.frame.height)
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override var prefersStatusBarHidden: Bool{
+        return false
+    }
 }
-
